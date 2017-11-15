394ab140d92e465eb87acbc823e44f3f05d41550
knutole <knutole@mapic.io>
2017-11-15 10:35:50 +0000

    improve graph data module

--- js/src/chrome/data/data.graph.js
+++ js/src/chrome/data/data.graph.js
@@ -1,15 +1,22 @@
 M.Data = M.Data || {};
 M.Data.Graph = M.Evented.extend({
 
+    DOM : {},
+    
+    data : {
+        geojson : '',
+        csv : []
+    },
+
     initialize : function () {
         console.log('M.Data.Graph');
 
-        this._create();
+        // create page
+        this._initContent();
 
     },
 
-
-    _create : function () {
+    _initContent : function () {
 
         // create fullscreen
         var fullscreen = this._fullscreen = new M.Fullscreen({
@@ -17,60 +24,73 @@ M.Data.Graph = M.Evented.extend({
             titleClassName : 'slim-font'
         });
 
-        this._fullscreen.data = {};
+        // store
+        this.DOM.fullscreen_content = this._fullscreen._content
 
-        // shortcuts
-        var content = this._fullscreen._content;
+        // create geojson input
+        this._createGeoJSONInput();
        
         // create graph layer item
-        this._createDataBox({
-            container : content,
-        });
-
-        var project = app.activeProject;
+        this._createCSVInput();
 
         // save button
         var save_button = M.DomUtil.create('div', 'smooth-fullscreen-save', content, 'Create Layer');
-        M.DomEvent.on(save_button, 'click', function () {
-
-            var options = {
-                projectUuid : project.getUuid(), // pass to automatically attach to project
-                data : {
-                    graph : M.stringify({
-                        geojson : this._fullscreen.data.geojson,
-                        csv : []
-                    })
-                },
-                // metadata : layer.options.metadata,  // TODO
-                title : 'test-geojson-title',
-                description : 'test-geojson-description',
-                file : null
-            };
-
-            app.api.createLayer(options, function (err, body) {
-                if (err) return done(err);
-                
-                var layerModel = M.parse(body);
-
-
-                // create layer on project
-                var layer = project.addLayer(layerModel);
-
-                // select project
-                M.Mixin.Events.fire('layerAdded', { detail : {
-                    projectUuid : project.getUuid(),
-                    layerUuid : layerModel.uuid
-                }});
-
-            });
-
-
-        }, this);
+        M.DomEvent.on(save_button, 'click', this._onSave, this);
 
     },
 
-    _createDataBox : function (options) {
-        var container = options.container;
+    _onSave : function () {
+
+        // get project
+        var project = app.activeProject;
+
+        var data = this.data;
+
+        console.log('_onSAve data', data);
+
+        // create layer @ api
+        app.api.createLayer({
+            projectUuid : project.getUuid(), // pass to automatically attach to project
+            data : {
+                graph : M.stringify({
+                    geojson : data.geojson,
+                    csv : data.csv
+                })
+            },
+            title : 'test-geojson-title',
+            description : 'test-geojson-description',
+            file : null,
+            // metadata : layer.options.metadata,  // TODO
+        }, 
+
+        // callback
+        function (err, body) {
+            if (err) return console.error(err, body);
+            
+            // parse
+            var layerModel = M.parse(body);
+
+            console.log('callback layerModel', layerModel);
+
+            // create layer on project
+            var layer = project.addLayer(layerModel);
+
+            // select project
+            M.Mixin.Events.fire('layerAdded', { detail : {
+                projectUuid : project.getUuid(),
+                layerUuid : layerModel.uuid
+            }});
+
+        });
+    },
+
+    _createGeoJSONInput : function (options) {
+
+        // get container
+        var container = this.DOM.fullscreen_content;
+
+        // get data
+        var data = this.data;
 
         // wrapper
         var toggles_wrapper = M.DomUtil.create('div', 'toggles-wrapper file-options', container);
@@ -85,28 +105,26 @@ M.Data.Graph = M.Evented.extend({
         upload_button.setAttribute('id', 'geojson-upload');
         upload_button.setAttribute('type', 'file');
 
-
-        var data = this._fullscreen.data;
-
+        // on upload
         M.DomEvent.on(upload_button, 'change', function () {
-                    
             var ctx = this;
-            
             var file = ctx.files[0];
-
             var reader = new FileReader();
-
             reader.onload = function (e) {
-
                 var geojson = M.parse(e.currentTarget.result);
 
+                // set geojson to data
                 data.geojson = geojson;
-
             }
-
             reader.readAsText(file);
-
         });
+
+    },  
+
+    _createCSVInput : function (options) {
+
+        // get container
+        var container = this.DOM.fullscreen_content;
 
         // graph item
         var graph_wrapper = M.DomUtil.create('div', 'toggles-wrapper file-options', container);
@@ -119,19 +137,37 @@ M.Data.Graph = M.Evented.extend({
         var name = M.DomUtil.create('div', 'smooth-fullscreen-name-label clearboth', graph_wrapper, 'Title');
         var name_input = M.DomUtil.create('input', 'smooth-input smaller-input ', graph_wrapper);
         name_input.setAttribute('placeholder', 'Y Axis label');
-        var csv_input = M.DomUtil.create('textarea', 'smooth-input smaller-input margin-top-20', graph_wrapper);
-        csv_input.setAttribute('placeholder', 'Paste .csv graph data');
+       
+        // upload csv button
+        var upload_button = M.DomUtil.create('input', 'smooth-fullscreen-save', graph_wrapper);
+        upload_button.setAttribute('id', 'geojson-upload');
+        upload_button.setAttribute('type', 'file');
 
-        M.DomEvent.on(csv_input, 'change keyup keydown', function () {
-            M.DomUtil.addClass(csv_input, 'graph-item');
-        }, this);
+        // data
+        var data = this.data;
+        data.csv = data.csv || [];
 
-        return toggles_wrapper;
+        // on upload 
+        M.DomEvent.on(upload_button, 'change', function () {
+            var ctx = this;
+            var file = ctx.files[0];
+            var reader = new FileReader();
+            reader.onload = function (e) {
+                var parsed_csv = Papa.parse(e.currentTarget.result);
+
+                if (_.size(parsed_csv.error)) {
+                    console.error('error parsing csv', parsed_csv); // todo
+                }
+
+                // push to stack
+                data.csv.push({
+                    y_axis_label : name_input.value,
+                    csv : parsed_csv
+                });
+            }
+            reader.readAsText(file);
+        });
     },
-
-
-
-
 
 });
 
